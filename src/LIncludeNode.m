1438e7e23c57f9022030f88820eedca5f9f079e9
Florian Schwaiger <fschwaiger@gmail.com>
2025-02-06 08:22:53 +0100

    feat: template inclusion with fallback option
    This enables template inclusion with fallback options. Specify a list of
    templates, and the first template that is found, will get rendered.

--- src/LIncludeNode.m
+++ src/LIncludeNode.m
@@ -17,17 +17,28 @@ classdef LIncludeNode < LNode
         end
         
         function str = render(self, context)
-            filename = string(evalin_struct(self.Expression, context));
-
-            if not(endsWith(filename, ".template"))
-                filename = filename + ".template";
+            filenames = string(evalin_struct(self.Expression, context, self.Fragment));
+            filenames = reshape(filenames, 1, []);
+            
+            mask = not(endsWith(filenames, ".template"));
+            filenames(mask) = filenames(mask) + ".template";
+            
+            template = [];
+            for filename = filenames
+                try
+                    template = LFileTemplate(filename);
+                    break
+                catch e
+                    if e.identifier == "Lobster:NoSuchTemplate"
+                        continue
+                    else
+                        rethrow(e);
+                    end
+                end
             end
             
-            try
-                template = LFileTemplate(filename);
-            catch
-                str = "";
-                return
+            if isempty(template)
+                error("lobster:NoSuchTemplate", "Could not find any of the templates: %s", jsonencode(filenames));
             end
             
             str = template.render(context);
